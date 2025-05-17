5760553b0904375570e11af8dbde5a1e3754b56f
Vashvi Rank <rankvashvi@gmail.com>
2025-05-17 07:36:23 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,7 +43,6 @@ import Footer from "./layout/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
 import AnalyticsTracker from "./components/AnalyticsTracker";
-// import Chatbot from "./components/Chatbot";
 
 const ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
 ReactGA.initialize(ID);
@@ -90,7 +89,6 @@ function App() {
             )
           }
         />
-        <Route path="/chatbot" element={<Chatbot />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/seller-register" element={<SellerRegister />} />
