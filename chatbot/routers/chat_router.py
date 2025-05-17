a33207d21d871bc60e0a46d4c5f1914cb62ddee4
Vashvi <rankvashvi@gmail.com>
2025-05-17 07:29:20 +0530

    removed chat

--- chatbot/routers/chat_router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from models.message_model import get_all_messages
-
-router = APIRouter()
-
-
-@router.get("/messages")
-def fetch_messages():
-    return get_all_messages()
